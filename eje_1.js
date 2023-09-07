482290c4d39cb12d46e3732a5bbf0d589a1862e5
Jhorman Alexander Culma Hermosa <119307144+Alex022315@users.noreply.github.com>
2023-09-07 08:28:29 -0500

    ADD: Ejercicio 1

---
+++ eje_1.js
@@ -0,0 +1,19 @@
+//1.	Elabora un algoritmo que calcule el sueldo promedio de un grupo de empleados y mostrar el nombre de los empleados que ganan más que el promedio.
+
+let Empleado1 = parseInt(prompt('Sueldo empleado 1'));
+let Empleado2 = parseInt(prompt('Sueldo empleado 2'));
+let Empleado3 = parseInt(prompt('Sueldo empleado 3'));
+let Empleado4 = parseInt(prompt('Sueldo empleado 4'));
+
+let PromedioSueldos = (Empleado1+Empleado2+Empleado3+Empleado4)/4;
+
+//let EmpleadoMP = 
+
+
+if (Empleado1, Empleado2, Empleado3, Empleado4 > PromedioSueldos) {
+
+    console.log('El empleado' + , );
+}
+
+
+

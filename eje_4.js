08b0a9c1a9caf05c95c3bc0d347b833ef056f404
Jhorman Alexander Culma Hermosa <119307144+Alex022315@users.noreply.github.com>
2023-09-07 09:01:56 -0500

    ADD: eje_4

---
+++ eje_4.js
@@ -0,0 +1,12 @@
+//4.	Realiza un algoritmo que muestre los números primos que hay entre 250 y 550
+
+NumP = 2
+
+for (let numeros = 250; i < array.length; i++) {
+    const element = array[index];
+    if ((numeros/2) = 0) {
+      let Primos = array
+        console.log('Los numeros primos que hay entre 250 y 550 son: ' + Primos)
+        
+    }
+}
